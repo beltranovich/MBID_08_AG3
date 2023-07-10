e2e85e43e8d91b7ed5c98f85690341150d891d2b
beltranovich <102776048+beltranovich@users.noreply.github.com>
2023-07-10 00:49:00 -0500

    3 files modified

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-/ Import stylesheets
+// Import stylesheets
 import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 
+// Gráfico de columnas. Mi 1º gráfico
 var visualization = d3plus.viz()
 .container("#viz1")
 .data([{"year": 1991, "name":"alpha", "value": 15},
-{"year": 1992, "name":"alpha", "value": 20},])
+      {"year": 1992, "name":"alpha", "value": 20},])
 .type("bar")
 .id("name")
 .x("year")
 .y("value")
 .draw()
 
+// Cargar Datos desde una URL en formato JSON
 d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
 var visualization = d3plus.viz()
 .container("#viz2")
@@ -26,9 +28,9 @@ var visualization = d3plus.viz()
 .draw();
 });
 
-var visualization = d3plus.viz()
-  .container("#viz3")
-  .data([{"year": 1991, "name":"alpha", "value": 15},
+// Creación de variable data para la tercera visualización
+var data = [
+  {"year": 1991, "name":"alpha", "value": 15},
   {"year": 1991, "name":"beta", "value": 10},
   {"year": 1991, "name":"gamma", "value": 5},
   {"year": 1991, "name":"delta", "value": 50},
@@ -43,9 +45,15 @@ var visualization = d3plus.viz()
   {"year": 1994, "name":"alpha", "value": 60},
   {"year": 1994, "name":"beta", "value": 60},
   {"year": 1994, "name":"gamma", "value": 25},
-  {"year": 1994, "name":"delta", "value": 32},])
-  .type("box")
-  .id("name")
-  .x("year")
-  .y("value")
-  .draw()
+  {"year": 1994, "name":"delta", "value": 32}
+]
+
+// Visualización de Box Plot
+var visualization = d3plus.viz()
+.container("#viz3")
+.data(data)
+.type("box")
+.id("name")
+.x("year")
+.y("value")
+.draw()
